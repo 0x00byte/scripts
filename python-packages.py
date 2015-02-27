74b6dab5f365e67ed8b0d63a5432e5a183979f29
0x00byte <0x00byte@0x00bytes-MacBook-Pro.local>
2015-02-27 17:55:54 +0100

    Minor changes

--- python-packages.py
+++ python-packages.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
+print "The following packages are currently installed:"
 import pip
 installed_packages = pip.get_installed_distributions()
 installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
-print 'The following packages are currently installed:'
-     for i in installed_packages])
+	for i in installed_packages])
 print installed_packages_list
