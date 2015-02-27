c35f60c224612f408f5f846d5ad2862032010789
0x00byte <0x00byte@6c6f6c.net>
2015-02-27 23:16:54 +0100

    Changed print msg

--- client-probe.py
+++ client-probe.py
@@ -8,16 +8,16 @@ clientprobes = set()
 def PacketHandler(pkt) :
 
 	if pkt.haslayer(Dot11ProbeReq) :
-	
+
 		if len(pkt.info) > 0 :
 			testcase = pkt.addr2 + '---' + pkt.info
 			if testcase not in clientprobes :
 				clientprobes.add(testcase)
-				print "New Probe Found: " + pkt.addr2 + ' ' + pkt.info + "\n"	
-				                                                                             
-				print "     000000000                              000000000          000000000"     
-				print "   00:::::::::00                          00:::::::::00      00:::::::::00"   
-				print " 00:::::::::::::00                      00:::::::::::::00  00:::::::::::::00" 
+				print "New Probe Found: " + pkt.addr2 + ' ' + pkt.info + "\n"
+
+				print "     000000000                              000000000          000000000"
+				print "   00:::::::::00                          00:::::::::00      00:::::::::00"
+				print " 00:::::::::::::00                      00:::::::::::::00  00:::::::::::::00"
 				print "0:::::::000:::::::0                    0:::::::000:::::::00:::::::000:::::::0"
 				print "0::::::0   0::::::0xxxxxxx      xxxxxxx0::::::0   0::::::00::::::0   0::::::0"
 				print "0:::::0     0:::::0 x:::::x    x:::::x 0:::::0     0:::::00:::::0     0:::::0"
@@ -31,15 +31,15 @@ def PacketHandler(pkt) :
 				print " 00:::::::::::::00   x:::::x  x:::::x   00:::::::::::::00  00:::::::::::::00"
 				print "   00:::::::::00    x:::::x    x:::::x    00:::::::::00      00:::::::::00"
 				print "     000000000     xxxxxxx      xxxxxxx     000000000          000000000"
-				
-				print "\n--------- HERE ARE SOME AP'S MO-FO  ----------------\n"
-                                                                         
+
+				print "\n------------- ACCESS POINTS  ----------------\n"
+
 				counter = 1
 				for probe in clientprobes:
 					[client, ssid] = probe.split('---')
 					print counter, client, ssid
 					counter = counter + 1
-				
+
 				print "\n---------------------------------------------\n"
 
 sniff(iface = sys.argv[1], count = int(sys.argv[2]), prn = PacketHandler)
